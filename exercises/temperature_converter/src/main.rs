9cad0adfc2d218683a964280aaf503c2a3440f2f
Lucas Ambrosi Derrossi <lucas_ambrosi@yahoo.com.br>
2020-04-26 18:43:11 -0300

    Fix function name

--- exercises/temperature_converter/src/main.rs
+++ exercises/temperature_converter/src/main.rs
@@ -1,15 +1,15 @@
 fn main() {
     let celsius = 40.0;
-        println!("{}°C is {}°F", celsius, celsius_to_fahrenheit(celsius));
+        println!("{}°C is {}°F", celsius, to_fahrenheit(celsius));
 
     let fahrenheit = 104.0;
-    println!("{}°F is {}°C", fahrenheit, fahrenheit_to_celsius(fahrenheit));
+    println!("{}°F is {}°C", fahrenheit, to_celsius(fahrenheit));
 }
 
-fn celsius_to_fahrenheit(celsius: f64) -> f64 {
+fn to_fahrenheit(celsius: f64) -> f64 {
     (celsius / 5.0) * 9.0 + 32.0
 }
 
-fn fahrenheit_to_celsius(fahrenheit: f64) -> f64 {
+fn to_celsius(fahrenheit: f64) -> f64 {
     (fahrenheit - 32.0) * 5.0 / 9.0
 }
