03d93cb1627497b8263d6468ebb39d362658cc34
Lucas Ambrosi Derrossi <lucas_ambrosi@yahoo.com.br>
2020-05-15 01:01:42 -0300

    Add tuple struct

--- playground/structs/src/main.rs
+++ playground/structs/src/main.rs
@@ -38,3 +38,12 @@ fn struct_update_syntax() {
         ..user1
     };
 }
+
+// ===== Tuple Structs =====
+fn tuple_structs() {
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+}
