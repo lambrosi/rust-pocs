0364000d48196b95ec7eb06aee12f7953759c2d6
Lucas Ambrosi Derrossi <lucas_ambrosi@yahoo.com.br>
2020-05-17 16:49:36 -0300

    Add sample struct

---
+++ exercises/rectangle_area/src/main.rs
@@ -0,0 +1,47 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size
+        }
+    }
+}
+
+fn main() {
+    let rect = Rectangle {
+        width: 30,
+        height: 50
+    };
+
+    println!("Rectangle input: {:?}", rect);
+
+    println!("The area of the rectangle is {}.", rect.area());
+
+    // --------------------------------------
+
+    let rect2 = Rectangle {
+        width: 25,
+        height: 40
+    };
+
+    println!("Rect can hold Rect2: {}", rect.can_hold(&rect2));
+
+    // --------------------------------------
+
+    let rect3 = Rectangle::square(5);
+    println!("The area of the rectangle is {}.", rect3.area());
+}
