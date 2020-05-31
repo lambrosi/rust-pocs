41da485c0da1762d4823505d2a1077fa1b6a1eba
Lucas Ambrosi Derrossi <lucas_ambrosi@yahoo.com.br>
2020-05-31 15:45:37 -0300

    Add struct to guessing_game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,6 +2,24 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
+struct Guess {
+    value: i32
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("The number shoud be between 1 and 100 (inclusive). Passed: {}.", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("== Guess the number ==");
 
@@ -15,14 +33,14 @@ fn main() {
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(number) => number,
+        let guess: Guess = match guess.trim().parse() {
+            Ok(number) => Guess::new(number),
             Err(_) => continue,
         };
 
-        println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess.value());
 
-        match guess.cmp(&secret_number) {
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big"),
             Ordering::Equal => {
