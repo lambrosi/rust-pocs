970fd818df5edea6a01f6a0c773f872d29cd0a09
Lucas Ambrosi Derrossi <lucas_ambrosi@yahoo.com.br>
2020-06-08 01:28:12 -0300

    Read the file

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Serching for '{}'", query);
+    println!("In file '{}'", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Error to read the file.");
+
+    println!("Content: {}", contents);
 }
