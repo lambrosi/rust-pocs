bf2c54a6d4602c058ae21b27828a6d18687968d8
Lucas Ambrosi Derrossi <lucas_ambrosi@yahoo.com.br>
2020-06-08 13:27:23 -0300

    Some refactor in minigrep

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,28 @@
+use std::fs;
+use std::error::Error;
+
+pub struct Config {
+    pub query: String,
+    pub filename: String
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Passed less than two parameters.");
+        }
+
+        Ok(Config {
+            query: args[1].clone(),
+            filename: args[2].clone()
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("Content: {}", contents);
+
+    Ok(())
+}
