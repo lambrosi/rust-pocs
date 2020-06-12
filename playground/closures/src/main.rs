cfaf1be77d79da988f7dab8afe0f1b0882203a6d
Lucas Ambrosi Derrossi <lucas_ambrosi@yahoo.com.br>
2020-06-11 23:49:53 -0300

    Add closure sample

--- playground/closures/src/main.rs
+++ playground/closures/src/main.rs
@@ -2,10 +2,15 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
+    let expensive_closure = |number| {
+        expensive_calculation(number)
+    };
 
+    println!("Result: {}", expensive_closure(15));
 }
 
 fn expensive_calculation(amount: u32) -> u32 {
+    println!("Perfoming calculation...");
     thread::sleep(Duration::from_secs(2));
     amount
 }
