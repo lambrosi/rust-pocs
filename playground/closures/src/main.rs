db9ef8a90f195b3c6b6795323b194bb3106ff91f
Lucas Ambrosi Derrossi <lucas_ambrosi@yahoo.com.br>
2020-06-12 00:15:49 -0300

    Adjust closure sample

--- playground/closures/src/main.rs
+++ playground/closures/src/main.rs
@@ -2,14 +2,29 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
+    let value = 10;
+
+    print_result(value, true);
+}
+
+fn print_result(value: u32, standard: bool) {
     let expensive_closure = |number| {
         expensive_calculation(number)
     };
 
     // Or without {}, because this are a simple expression
-    let exp = |num| expensive_calculation(num);
+    // let exp = |num| expensive_calculation(num);
 
-    println!("Result: {}", expensive_closure(15));
+    if value > 5 {
+        println!("First opt: {}", expensive_closure(value));
+        println!("Second opt: {}", expensive_closure(value));
+    } else {
+        if standard {
+            println!("Standard...");
+        } else {
+            println!("Customized: {}", expensive_closure(value));
+        }
+    }
 }
 
 fn expensive_calculation(amount: u32) -> u32 {
