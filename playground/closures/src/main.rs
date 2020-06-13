37a61d821523f84658e4740a5a34b29780d20960
Lucas Ambrosi Derrossi <lucas_ambrosi@yahoo.com.br>
2020-06-12 23:21:53 -0300

    Modify closure example

--- playground/closures/src/main.rs
+++ playground/closures/src/main.rs
@@ -7,22 +7,23 @@ fn main() {
     print_result(value, true);
 }
 
-fn print_result(value: u32, standard: bool) {
-    let expensive_closure = |number| {
-        expensive_calculation(number)
-    };
+fn print_result(intensity: u32, standard: bool) {
+    // If the expression have multiple lines, use {}
+    // let expensive_closure = |number| {
+    //     let n = number 2;
+    //     n * 4
+    // };
 
-    // Or without {}, because this are a simple expression
-    // let exp = |num| expensive_calculation(num);
+    let mut expensive_closure = Cacher::new(|num| expensive_calculation(num));
 
-    if value > 5 {
-        println!("First opt: {}", expensive_closure(value));
-        println!("Second opt: {}", expensive_closure(value));
+    if intensity > 5 {
+        println!("First opt: {}", expensive_closure.value(intensity));
+        println!("Second opt: {}", expensive_closure.value(intensity));
     } else {
         if standard {
             println!("Standard...");
         } else {
-            println!("Customized: {}", expensive_closure(value));
+            println!("Customized: {}", expensive_closure.value(intensity));
         }
     }
 }
@@ -31,4 +32,35 @@ fn expensive_calculation(amount: u32) -> u32 {
     println!("Perfoming calculation...");
     thread::sleep(Duration::from_secs(2));
     amount
+}
+
+struct Cacher<T>
+    where
+        T: Fn(u32) -> u32
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+    where
+        T: Fn(u32) -> u32
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
 }
