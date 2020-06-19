8d742b1b48593233c182c6d98aa8a273e7451b01
Lucas Ambrosi Derrossi <lucas_ambrosi@yahoo.com.br>
2020-06-18 22:59:22 -0300

    Add drop trait sample

---
+++ playground/smart_pointers_drop_trait/src/main.rs
@@ -0,0 +1,24 @@
+// Drop trait lets you customize what happens when a value is about to go out of scope
+// To force a drop before the end of scope, call the method std::mem::drop
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data '{}'", self.data);
+    }
+}
+
+fn main() {
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+
+    println!("CustomSmartPointers created.");
+}
